1810ec31edc8c0ee8f7bcdd410ac2b52c96a69a2
DoodMcGuy <minecraftrocks9@gmail.com>
2026-02-16 15:32:24 -0600

    randomly select a card from the list

--- index.tsx
+++ index.tsx
@@ -9,15 +9,27 @@ async function getCreature(cmc: string) {
     const reqUrl = new URL(baseUrl);
     reqUrl.searchParams.append('q', `t:creature in:paper cmc:${cmc}`);
 
-    const res = await fetch(reqUrl, {
-        method: 'GET'
-    });
+    const res = await fetch(reqUrl, {method: 'GET'});
 
     if (res.status !== 200) {
         throw Error('Request to Scryfall failed');
     }
     
     const body = await res.json();
-    const selectedCard = Math.floor(Math.random() * body.total_cards + 1) + 1;
-    return selectedCard;
+    const selectedCard = Math.floor(Math.random() * (body.total_cards - 1));
+    
+    // Scryfall API is paginated at 175 entries per page
+    if (selectedCard > 175) {
+        const page = Math.floor(selectedCard/175);
+        const pagedCard = selectedCard % 175;
+        
+        const pageRegex = /page=[1-9]?/gm;
+        const pagedUrl = body.next_page.replace(pageRegex, `page=${page}`);
+        const pagedRes = await fetch(new URL(pagedUrl), {method: 'GET'});
+        const pagedBody = await pagedRes.json();
+        
+        return pagedBody.data[pagedCard];    
+    }
+
+    return body.data[selectedCard];
 }
