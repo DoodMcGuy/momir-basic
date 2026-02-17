7107582d46cbe9180b1444aeb01637dd714b69ed
DoodMcGuy <minecraftrocks9@gmail.com>
2026-02-17 00:10:12 -0600

    throw error on 14 mv creature

--- index.tsx
+++ index.tsx
@@ -4,8 +4,9 @@ if (process.argv.length < 3) {
     throw Error('Missing CMC argument!');
 } 
 
-if (Number(process.argv[2]) > 16) {
-    throw Error('Mana value too high, womp womp');
+// No creatures over 16 cmc (draco is 16) or at 14 
+if (Number(process.argv[2]) > 16 || Number(process.argv[2]) === 14)  {
+    throw Error('Invalid mana value, womp womp');
 }
-getCreature(process.argv[2]).then((res) => console.log(res));
 
+getCreature(process.argv[2]).then((res) => console.log(res));
